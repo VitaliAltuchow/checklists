794556968bb467ce9d34274921144fcb25e78c18
Vitali <vitali.altuchow@alumni.fh-aachen.de>
2023-06-22 20:32:09 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import MyLists from './components/MyLists'
-import './App.css'
+import { useState } from "react";
+import MyLists from "./components/MyLists";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='header-container'>
-        <p className='heading'>check<span className='lists'>lists</span></p>
+      <div className="header-container">
+        <p className="header">
+          <span className="check">check</span>
+          <span className="lists">lists</span>
+        </p>
       </div>
-      <MyLists/>
+      <MyLists />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MyLists.jsx
+++ src/components/MyLists.jsx
@@ -1,103 +1,180 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function MyList({name, color}){
-    const [items, setItems] = useState([]);
-    const [textInput, setTextInput] = useState("");
+/**
+ * A list whose name and color is passed by the parameters.
+ * It is possible to insert any number of list entries, check them off and remove them.
+ */
+function MyList({ name, color }) {
+  const [items, setItems] = useState([]);
+  const [textInput, setTextInput] = useState("");
 
-    return (
-    <div className='list' style={{backgroundColor: color}}>
-    <h1>{name}</h1>
-    <ul>
-        {items.map(item => {
-            return (
+  return (
+    <div className="list" style={{ backgroundColor: color }}>
+      <h1>{name}</h1>
+      <ul>
+        {items.map((item) => {
+          return (
             <li key={item.key}>
-                <input type="checkbox" checked={item.checked} onClick={e => {
-                        setItems(items.map(item2 => {
-                            if(item2.key === item.key){
-                                return {
-                                    ...item2,
-                                    checked : !item2.checked
-                                };
-                            }else{
-                                return item2;
-                            }
-                        }));
-                    }}/>
-                    <span className={(item.checked ? "checked" : "") + ' item-name'}>{item.name}</span>
-            </li>);
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onClick={(e) => {
+                  setItems(
+                    items.map((item2) => {
+                      if (item2.key === item.key) {
+                        return {
+                          ...item2,
+                          checked: !item2.checked,
+                        };
+                      } else {
+                        return item2;
+                      }
+                    })
+                  );
+                }}
+              />
+              <span className={(item.checked ? "checked" : "") + " item-name"}>
+                {item.name}
+              </span>
+            </li>
+          );
         })}
         <li>
-        <input type="text" className='inputNewItem' value={textInput} onKeyDown={(e) => {
-            if(e.key === 'Enter'){
-                
-            if(! items.find(item => item.key === textInput) && textInput != ''){
-                setItems([...items, {
+          <input
+            type="text"
+            className="inputNewItem"
+            value={textInput}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (
+                  !items.find((item) => item.key === textInput) &&
+                  textInput != ""
+                ) {
+                  setItems([
+                    ...items,
+                    {
+                      key: textInput,
+                      name: textInput,
+                      checked: false,
+                    },
+                  ]);
+                }
+                setTextInput("");
+              }
+            }}
+            onChange={(e) => {
+              setTextInput(e.target.value);
+            }}
+          />
+          <button
+            className="plus-button"
+            onClick={() => {
+              if (
+                !items.find((item) => item.key === textInput) &&
+                textInput != ""
+              ) {
+                setItems([
+                  ...items,
+                  {
                     key: textInput,
                     name: textInput,
-                    checked: false
-            }]);
-            }
-            setTextInput("");
-            }
-        }}  onChange={(e) => {setTextInput(e.target.value)}}/>
-        <button className='plus-button' onClick={() => {
-            if(! items.find(item => item.key === textInput) && textInput != ''){
-                setItems([...items, {
-                    key: textInput,
-                    name: textInput,
-                    checked: false
-            }]);
-            }
-            setTextInput("");
-        }}>
+                    checked: false,
+                  },
+                ]);
+              }
+              setTextInput("");
+            }}
+          >
             +
-        </button>
+          </button>
         </li>
-    </ul>
-        
-        {items.find(item => item.checked === true) && 
-        <button className='text-button delete-button' onClick={() => {
-            setItems(items.filter(item2 => !item2.checked));
-        }}>
-            Delete Checked Items
-        </button>}
+      </ul>
 
-    </div>);
+      {items.find((item) => item.checked === true) && (
+        <button
+          className="delete-button"
+          onClick={() => {
+            setItems(items.filter((item2) => !item2.checked));
+          }}
+        >
+          Delete Checked Items
+        </button>
+      )}
+    </div>
+  );
 }
-export default function MyLists(){
 
-    const [lists, setLists] = useState([]);
-    const [formTextInput, setFormTextInput] = useState("");
-    const [newListForm, setNewListForm] = useState(false);
-    const [selectedColor, setSelectedColor] = useState('#c5c5c5');
+/**
+ * Container with the possibility to create as many lists (MyList Components) as you want.
+ */
+export default function MyLists() {
+  const grey = "#c5c5c5";
+  const [lists, setLists] = useState([]);
+  const [formTextInput, setFormTextInput] = useState("");
+  const [newListForm, setNewListForm] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(grey);
 
-   return (<div className='list-container'>
-    {lists}
-    
-    {!newListForm && <button className='plus-button' onClick={e => {
-        setNewListForm(true);
-    }}>+</button>}
+  return (
+    <div className="list-container">
+      {lists}
 
-    {newListForm && <div className='list' style={{backgroundColor: selectedColor}}>
-        <input placeholder='Title' className='inputNewList' type='text' onKeyDown={(e) => {
-            if(e.key === 'Enter' && formTextInput != ''){
-                setLists([...lists, <MyList name={formTextInput} color={selectedColor}></MyList>])
-            setNewListForm(false);
-            setFormTextInput("");
-            setSelectedColor('#c5c5c5');
-            }
-        }} value={formTextInput}
-            onChange={(e) => {setFormTextInput(e.target.value);}}/>
-        <input className='color-picker' type='color' value={selectedColor} onChange={e => setSelectedColor(e.target.value)}/>
-        <button className='newListSubmitButton text-button' onClick={e => {
-            if(formTextInput != ''){
-                setLists([...lists, <MyList name={formTextInput} color={selectedColor}></MyList>])
+      {!newListForm && (
+        <button
+          className="plus-button"
+          onClick={(e) => {
+            setNewListForm(true);
+          }}
+        >
+          +
+        </button>
+      )}
+
+      {newListForm && (
+        <div className="list" style={{ backgroundColor: selectedColor }}>
+          <input
+            placeholder="Title"
+            className="inputNewList"
+            type="text"
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && formTextInput != "") {
+                setLists([
+                  ...lists,
+                  <MyList name={formTextInput} color={selectedColor}></MyList>,
+                ]);
                 setNewListForm(false);
                 setFormTextInput("");
-                setSelectedColor('#c5c5c5');
-            }
-        }}>Create List</button>
-    </div>}
-    
-    </div>);
+                setSelectedColor(grey);
+              }
+            }}
+            value={formTextInput}
+            onChange={(e) => {
+              setFormTextInput(e.target.value);
+            }}
+          />
+          <input
+            className="color-picker"
+            type="color"
+            value={selectedColor}
+            onChange={(e) => setSelectedColor(e.target.value)}
+          />
+          <button
+            className="newListSubmitButton"
+            onClick={(e) => {
+              if (formTextInput != "") {
+                setLists([
+                  ...lists,
+                  <MyList name={formTextInput} color={selectedColor}></MyList>,
+                ]);
+                setNewListForm(false);
+                setFormTextInput("");
+                setSelectedColor(grey);
+              }
+            }}
+          >
+            Create List
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
